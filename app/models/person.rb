0bf1302fca0ae0217d2e084e0748f392072308be
Maloney L <maloneyl@users.noreply.github.com>
2019-02-07 14:40:42 +0000

    try a different way of ordering

--- app/models/person.rb
+++ app/models/person.rb
@@ -9,6 +9,10 @@ class Person < ActiveRecord::Base
   end
 
   def self.alphabetically_by_region_and_location
-    joins(location: :region).order("regions.name, locations.name, name")
+    # joins(location: :region).order("regions.name, locations.name, name")
+    joins(location: :region).
+      merge(Region.order(:name)).
+      merge(Location.order(:name)).
+      order(:name)
   end
 end
